e91c36c2e5d854a9e6d4aca9c9ef4ce1774a08c9
김남호 <kimnamo0113@gmail.com>
2019-06-21 15:26:50 +0900

    Main 수정

--- src/jdbc_study/ErpMain.java
+++ src/jdbc_study/ErpMain.java
@@ -9,16 +9,18 @@ public class ErpMain {
 
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator"));
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					ErpManagementUI frame = new ErpManagementUI();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
+		System.out.println("첫 번째 수정");
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ErpManagementUI frame = new ErpManagementUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
